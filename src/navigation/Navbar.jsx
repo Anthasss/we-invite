4f9c3a97426f9edb0aca5bed58af27480e231aca
Anthasss <anthasprog@gmail.com>
2025-09-30 17:52:51 +0800

    added weinvite theme

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -3,7 +3,7 @@ import menuItems from "./../json/menuItems.json";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-300 w-full">
+    <div className="navbar bg-transparent w-full">
       <div className="flex-none lg:hidden">
         <label
           htmlFor="my-drawer-3"
@@ -26,13 +26,13 @@ export default function Navbar() {
         </label>
       </div>
       <div className="mx-2 flex-1 px-2">
-        <Link to="/" className="btn btn-ghost text-xl">We Invite</Link>
+        <Link to="/" className="btn bg-transparent hover:bg-stone-900/20 text-xl border-none">We Invite</Link>
       </div>
       <div className="hidden flex-none lg:block">
         <ul className="menu menu-horizontal">
           {menuItems.map((item) => (
             <li key={item.title}>
-              <Link to={item.link}>{item.title}</Link>
+              <Link className="btn bg-transparent hover:bg-stone-900/20 border-none" to={item.link}>{item.title}</Link>
             </li>
           ))}
         </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,7 @@
 export default function Home() {
   return (
-    <div className="p-6">
-      <div className="hero bg-base-200 rounded-lg">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Welcome!</h1>
-            <p className="py-6">
-              This is the home page of your We-Invite application. Start exploring the different sections using the navigation menu.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <h1 className="text-3xl font-bold underline">Home Page</h1>
     </div>
   );
 }
